bca989cb2ca08d9b475a8259768ea06759c4ce0d
Zinnig <38312982+Zinnig@users.noreply.github.com>
2020-01-20 09:04:31 +0100

    Fixed Bugs

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+import { Client } from 'discord.js';
+const client = new Client();
 
 const token = process.env.token;
-var $ = require('jquery');
+import $ from 'jquery';
 
 client.login(token);
 
@@ -50,8 +50,8 @@ client.on("voiceStateUpdate", () =>{
     const guild = client.guilds.get('463736564837777428')
     const channels = guild.channels.filter(c => c.parentID === '468697649592401920' && c.type === 'voice');
 
-    for (const [channelID, channel] of channels) {
-    for (const [memberID, member] of channel.members) {
+    for (const channel of channels) {
+    for (const member of channel.members) {
         if(channel.id === '666379507522863104'){
         member.setVoiceChannel('666381898343514133')
         .then(() => console.log(`Moved ${member.user.tag}.`))

931bb4e817ca6bdcee685a90106fd7f6f2b10f97
Zinnig <38312982+Zinnig@users.noreply.github.com>
2020-08-04 15:30:31 +0200

    "Removed" gxpleaderboard so the bot doesn't crash

--- main.js
+++ main.js
@@ -68,113 +68,91 @@ client.on("message", async message => {
             message.author.send(commandEmbed)
         }
         
-        if(cmd == "gxpleaderboard"){
-    
-        var mostGxpList = [];
-        var mostGxpListMult = [];
-        let resText1 = "";
-        let resText2 = "";
-        let gxp10 = ""
-        let sent4 = false;
-    
-        console.log("A")
+        /* if(cmd == "gxpleaderboard"){
+            var mostGxpList = [];
+            var mostGxpListMult = [];
+            let resText1 = "";
+            let resText2 = "";
             let xml = new XMLHttpRequest();
             xml.open("GET", "https://api.wynncraft.com/public_api.php?action=statsLeaderboard&type=guild&timeframe={}");
             xml.onreadystatechange = function(){
-            if(xml.status == 200){
+            if(this.status == 200){
                 try{
                 resText1 = JSON.parse(this.responseText);
-                let xml1;
-                console.log("putINList")
-                for(i=0;i<100;i++){
-                    xml1 = new XMLHttpRequest();
-                    xml1.open("GET", "https://api.wynncraft.com/public_api.php?action=guildStats&command=" + resText1["data"][i]["name"]);
-                    xml1.send();
-                    xml1.onreadystatechange = function(){
-                            if(xml1.status == 200){
-                                try{
-                                        resText2 = JSON.parse(this.responseText);
-                                        alreadyrequested.push(i)
-                                        console.log("getGxp")
-                                        try{
-                                        for(i=0;i<resText2["members"].length;i++){
-                                            contributedMember = parseInt(resText2.members[i].contributed)
-                                            member = resText2.members[i].name;
-                                            tag = resText2.prefix
-                                            if(mostGxpList.indexOf(contributedMember) == -1){
-                                                mostGxpList.push(contributedMember);
-                                            }
-                                            if(index(contributedMember, mostGxpListMult) == -1){ 
-                                                mostGxpListMult.push([member, tag, contributedMember]);
-                                            }
-                                        }
-                                        }catch(e){}
-                                        mostGxpList.sort(function(a, b){
-                                    
-                                            return b - a;
-                                        
-                                        });
-                                        mostGxpListMult.sort(function(a, b){
-                                            return b[2] - a[2];
-                                        });
-                                        
-                                }catch(e){}
-                                completed = [...new Set(alreadyrequested)];
-                                console.log("already:" + alreadyrequested.length)
-                                console.log(completed.length)
-                                if(alreadyrequested.length >= 1){
-                                    console.log("after for")
-                                    console.log(mostGxpListMult)
-                                        if(sent4 == false){
-                                            for (x = 0; x < 25; x++){
-                                                console.log("a")
-                                                gxp10 += (x+1) + ". " +  mostGxpListMult[x][0]+ "[" + mostGxpListMult[x][1] + "]" +": "+ formatNumber(mostGxpList[x]) + "\n";
-                                                }
-                                                message.channel.send("**Gxp-Leaderboard (not 100% accurate)**")
-                                                message.channel.send(gxp10)
-                                                message.channel.send("owo")
-                                                sent4 = true;
-                                                            }
-                        }    
-                            
-                    }  
-                    
-            
-               
-                
-            }
-                }
-                
+                putInList(resText1);
             }catch(e){}
         }
         }
         xml.send();
-        
-        function index(contributed, arr) {
-            for (var i=0; i<arr.length; i++) {
-            for (var j=0; j<arr[i].length; j++) {
-            if (arr[i][j] == contributed) { return i; }
-            }
-            }
-            return -1;
         }
-        function formatNumber(num) {
-            return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-          }
-        
-        function getGxp(guild){
-           
+            function index(contributed, arr) {
+                for (var i=0; i<arr.length; i++) {
+                for (var j=0; j<arr[i].length; j++) {
+                if (arr[i][j] == contributed) { return i; }
+                }
+                }
+                return -1;
+            }
+            function formatNumber(num) {
+                return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+              }
+            let gxp10 = ""
+            function getGxp(guild){
+                try{
+                for(i=0;i<guild["members"].length;i++){
+                    contributedMember = parseInt(guild.members[i].contributed)
+                    member = guild.members[i].name;
+                    tag = guild.prefix
+                    if(mostGxpList.indexOf(contributedMember) == -1){
+                        mostGxpList.push(contributedMember);
+                    }
+                    if(index(contributedMember, mostGxpListMult) == -1){ 
+                        mostGxpListMult.push([member, tag, contributedMember]);
+                    }
+                }
+                }catch(e){}
+                mostGxpList.sort(function(a, b){
             
-        }
-        let alreadyrequested = []
-        function putInList(data){
-            
-            
-            
-        };
-             
-            
-    };    
+                    return b - a;
+                
+                });
+                mostGxpListMult.sort(function(a, b){
+                    return b[2] - a[2];
+                });
+            }
+            let alreadyrequested = []
+            function putInList(data){
+                let xml1
+                
+                for(i=0;i<100;i++){
+                        xml1 = new XMLHttpRequest();
+                        xml1.open("GET", "https://api.wynncraft.com/public_api.php?action=guildStats&command=" + data["data"][i]["name"]);
+                        xml1.onreadystatechange = function(){
+                                if(this.status == 200){
+                                    try{
+                                        resText2 = JSON.parse(this.responseText);
+                                        alreadyrequested.push(i)
+                                        getGxp(resText2); 
+                                        gxp10 = ""
+                for (i = 0; i < 100; i++){
+                    gxp10 += (i+1) + ". " +  mostGxpListMult[i][0]+ "[" + mostGxpListMult[i][1] + "]" +": "+ formatNumber(mostGxpList[i]) + "\n" ;
+                }
+                message.channel.send(gxp10);    
+                                        
+                                    }catch(e){}
+                                    
+                                    
+                                
+                        }
+                        
+                }
+                xml1.send();      
+                }};
+               
+                
+                
+                 */
+               
    
     let list = ["Avos Temple", "Bloody Beach", "Corkus Castle", "Corkus City", "Corkus City Mine",
 "Corkus City South", "Corkus Countryside", "Corkus Docks", "Corkus Forest North", 

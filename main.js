5afcaa3748e2c31b2bae9872a477625471580f3f
Mahakadema <70533348+Mahakadema@users.noreply.github.com>
2021-06-10 01:36:54 +0200

    add tracking for EMs and Tomes

--- main.js
+++ main.js
@@ -314,3 +314,190 @@ client.on("voiceStateUpdate", () => {
         }
     }
 })
+
+const loop = async () => {
+    let nextLbUpd = Date.now(); //1 hr, 30 mins ago
+    while (true) {
+        if (Date.now() > nextLbUpd) {
+            const lb = await utils.getGuildLeaderboard();
+            if (lb) {
+                //find time of next update
+                nextLbUpd = Date.now() - 2400000;
+                let newestReward = 0;
+                for (const guild of lb.data) {
+                    if (guild.rewards) {
+                        for (const reward of guild.rewards) {
+                            const time = Date.parse(reward.acquired);
+                            if (time > newestReward) {
+                                console.log(`New time found: ${time} (+${time - newestReward})`);
+                                newestReward = time;
+                            }
+                        }
+                    }
+                }
+                if (newestReward > nextLbUpd) {
+                    console.log(`Using reward timers: ${newestReward} (+${newestReward - nextLbUpd})`);
+                    nextLbUpd = newestReward;
+                }
+                nextLbUpd += 6000000; //1 hr, 40 minutes
+                console.log(`Next lbUpdate will be at ${nextLbUpd}, ${nextLbUpd - Date.now()} milliseconds`);
+                /**
+                 * So the way leaving members work is kinda e.
+                 * If a member leaves, their entry here gets deleted.
+                 * If they rejoin within 2 weeks (for example from a subguild),
+                 * we can manually check their previous value, then set their
+                 * "atLastTome: 0,Tome" property to the negative of that number
+                 */
+                //update PUN
+                const pun = lb.data.find(guild => guild.name === "Paladins United");
+                const oldRewards = JSON.parse(fs.readFileSync("./data/rewardData.json", "utf-8"));
+                const newRewards = {
+                    emeralds: [],
+                    guildTomes: [],
+                    members: [],
+                    tomesReserved: oldRewards.tomesReserved,
+                    currentTomeRule: oldRewards.currentTomeRule,
+                    lastTome: oldRewards.lastTome
+                };
+                const guild = await utils.getGuild();
+                //update members
+                for (const member of guild) {
+                    const old = oldRewards.members.find(m => m.uuid === member.uuid);
+                    if (old === undefined) {
+                        //create new
+                        newRewards.members.push({
+                            uuid: member.uuid,
+                            atLastTome: 0,
+                            previousTomes: []
+                        });
+                    } else {
+                        //copy
+                        newRewards.members.push(old);
+                    }
+                    //delete old entries
+                    const idx = newRewards.members.length - 1;
+                    while (newRewards.members[idx].previousTomes[0]?.time + 5184000000 < Date.now()) {
+                        newRewards.members[idx].previousTomes.shift();
+                    }
+                }
+                const qmNotes = client.channels.cache.get("671755757536018453"); //qm notes is "671755757536018453"
+                for (const emeralds of pun.rewards.filter(r => r.type === "EMERALDS")) {
+                    const foundEMAt = Date.parse(emeralds.acquired);
+                    const oldIndex = oldRewards.emeralds.findIndex(r => r.acquired === foundEMAt);
+                    newRewards.emeralds.push({
+                        acquired: foundEMAt
+                    })
+                    if (oldIndex < 0) {
+                        //emerald batch is new
+                        if (newRewards.emeralds.length === 7) {
+                            //storage critical, notify QMs
+                            qmNotes.send(`💵 The Emerald storage is filling up (14336/20480)! Please empty it soon!`).catch(e => {
+                                console.log(`Failed to notify Quartermasters:\n${e.stack}`);
+                            });
+                        } else if (newRewards.length === 10) {
+                            //storage full, notify QMs
+                            qmNotes.send(`💵 The Emerald storage has filled up! Please empty it immediately!`).catch(e => {
+                                console.log(`Failed to notify Quartermasters:\n${e.stack}`);
+                            });
+                        }
+                    }
+                }
+                for (const tome of pun.rewards.filter(r => r.type === "GUILD_TOMES")) {
+                    const foundTomeAt = Date.parse(tome.acquired);
+                    const oldIndex = oldRewards.guildTomes.findIndex(r => r.acquired === foundTomeAt);
+                    newRewards.guildTomes.push({
+                        acquired: foundTomeAt
+                    })
+                    if (oldIndex < 0) {
+                        //tome is new
+                        newRewards.lastTome++;
+                        if (newRewards.tomesReserved > 0) {
+                            //event tome or similar
+                            newRewards.tomesReserved--;
+                            qmNotes.send(`📘 A tome has been found! It has been declared as an off-rule reward.`).catch(e => {
+                                console.log(`Failed to notify Quartermasters:\n${e.stack}`);
+                            });;
+                        } else {
+                            let winner = "ERROR";
+                            let winnerUUID = "";
+                            const rule = newRewards.currentTomeRule;
+                            switch (newRewards.currentTomeRule) {
+                                case "WARS":
+                                    const warrers = [
+                                        "6d4dd862-a9f6-4171-9b62-fe78179b38e5", //Zinnig
+                                        "cf406197-f8e9-451f-870b-1cc2207d74ff", //Blockfox_XV
+                                        "b1b8d770-62a4-44e5-a6e8-68d8c7104ce8", //VHoltz_
+                                        "65b6835b-1af0-457e-8b1c-00239d8740e1", //Nieke
+                                        "5ce339c6-bb60-4142-8a50-4aa5ef0ef256", //Koni75
+                                        "05d9fea7-cfcc-4497-b77b-e326d1d2b42b", //MigatteNoGokuii
+                                        "47b48cdd-a4d2-45cf-a8a9-418c8d0be7b8", //Saraldar
+                                        "72d140ad-efe3-4bbe-b09c-df5e988f5332", //thyme23
+                                        "e67d4a1a-e879-4723-a356-0da0d15fe583", //g17fcH_3D
+                                        "1168c5ec-a8bf-45ac-ab9c-808a2b023364", //Mehku
+                                        //possible additions: Hrt1, Henry, Needsticc, The_Doggo
+                                    ];
+                                    const eligible = [];
+                                    for (const member of newRewards.members) {
+                                        if (warrers.includes(member.uuid) && member.previousTomes.length * 2 < newRewards.lastTome - (member.previousTomes[member.previousTomes.length - 1]?.id || 0)) {
+                                            eligible.push(member.uuid);
+                                        }
+                                    }
+                                    winnerUUID = eligible[Math.floor(Math.random() * eligible.length)];
+                                    newRewards.currentTomeRule = "GXP";
+                                    break;
+                                case "GXP":
+                                    const xp = [];
+                                    for (let i = 0; i < guild.length; i++) {
+                                        xp.push({
+                                            uuid: guild[i].uuid,
+                                            xp: guild[i].contributed - newRewards.members[i].atLastTome,
+                                            cooldown: newRewards.members[i].previousTomes.length * 2,
+                                            lastTome: newRewards.members[i].previousTomes[newRewards.members[i].previousTomes.length - 1]?.id | 0
+                                        });
+                                    }
+                                    xp.sort((a, b) => {
+                                        //if a is ineligible, return -1
+                                        if (a.cooldown >= newRewards.lastTome - a.lastTome && b.cooldown < newRewards.lastTome - a.lastTome) {
+                                            return -1;
+                                        }
+                                        //if a is ineligible, return -1
+                                        if (a.cooldown < newRewards.lastTome - a.lastTome && b.cooldown >= newRewards.lastTome - a.lastTome) {
+                                            return 1;
+                                        }
+                                        //default compare
+                                        return a.xp - b.xp;
+                                    });
+                                    winnerUUID = xp[xp.length - 1].uuid;
+                                    const winnerIdx = newRewards.members.findIndex(m => m.uuid === winnerUUID);
+                                    newRewards.members[winnerIdx].atLastTome = guild.find(m => m.uuid === winnerUUID).contributed;
+                                    newRewards.currentTomeRule = "WARS";
+                                    break;
+                            }
+                            winner = guild.find(mem => mem.uuid === winnerUUID).name;
+                            const winnerIndex = newRewards.members.findIndex(m => m.uuid === winnerUUID);
+                            if (winnerIndex >= 0) {
+                                newRewards.members[winnerIndex].previousTomes.push({
+                                    reason: rule,
+                                    time: Date.parse(tome.acquired),
+                                    id: newRewards.lastTome
+                                });
+                            } else {
+                                console.log(`${winnerUUID} does not appear to be in the guild`);
+                            }
+                            const msg = `📘 A tome has been found! The person to get it is **${winner}**.\n**Rule:** ${rule}`;
+                            qmNotes.send(msg).catch(e => {
+                                console.log(`Failed to send message\n${msg}\nin #qm-notes:\n${e}`);
+                            });
+                        }
+                    }
+                }
+                //save
+                fs.writeFileSync("./data/rewardData.json", JSON.stringify(newRewards, null, 2));
+            }
+            //repeat in 60s
+            await utils.sleep(60000);
+        }
+    }
+}
+
+loop();

8d7f7108feeb3a08780fdd1ad33d4f0f2ab5c8f8
Mahakadema <70533348+Mahakadema@users.noreply.github.com>
2021-06-10 01:29:12 +0200

    added reserveTome

---
+++ commands/reserveTome.js
@@ -0,0 +1,34 @@
+
+const Discord = require('discord.js');
+const fs = require("fs");
+function errorResponse(type, extraInfo){
+    let errorEmbed = new Discord.MessageEmbed()
+    .setColor("#ff0000")
+    switch(type){
+        case "noperms":
+            errorEmbed.setTitle(`You don't have the permission "${extraInfo}".`)
+            break;
+        case "wrongargs":
+            errorEmbed.setTitle(`Invalid Arguments! Valid Arguments: ${extraInfo}`)
+    }
+    return errorEmbed
+}
+
+module.exports = {
+	name: 'reservetome',
+	description: "Mark the next tomes to be given out on a custom rule.",
+	execute(message, args) {
+        if(message.member.hasPermission("MANAGE_GUILD")){
+            let amount = Number(args[0]);
+            if (Number.isNaN(amount) {
+                amount = 1;
+            }
+            const data = JSON.parse(fs.readFileSync("../data/rewardData.json", "utf-8"));
+            data.tomesReserved += amount;
+            fs.writeFileSync("../data/rewardData.json", JSON.stringify(data, null, 2));
+            message.channel.send((new Discord.MessageEmbed()).setTitle("reserveTome").setDescription(`**${amount}** tomes have been marked to be off-rule`));
+        }else{
+            message.channel.send(errorResponse("noperms", "MANAGE_GUILD"))
+        }
+    } 
+};

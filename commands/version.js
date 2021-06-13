c95a4848531f567b60d6f4ba3b3ff3c00ec96d96
Zinnig <38312982+Zinnig@users.noreply.github.com>
2021-06-13 22:37:12 +0200

    Improved Version command

--- commands/version.js
+++ commands/version.js
@@ -1,15 +1,23 @@
 
 const Discord = require('discord.js');
+const utils = require('../utils.js');
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
 
 module.exports = {
 	name: 'version',
 	description: "Gives general info about the bot",
-	execute(message, args, client) {
-        const embed = new Discord.MessageEmbed();
-        embed.setTitle("version");
-        embed.setDescription(`**Name:** Bot in a Crow's Nest
+	async execute(message, args, client) {
+        const $ = cheerio.load(await fetch(`https://github.com/Zinnig/Bot-in-a-crow-s-nest`).then(r => r.text()));
+        const data = $('body > div.application-main > div > main > div.container-xl > div > div > div.gutter-condensed > div.col-md-9 > div.Box > div.Box-header > div.js-details-container > div.flex-shrink-0 > ul > li > a > span > strong').text();
+        const embed = new Discord.MessageEmbed()
+        .setTitle(`Bot in a Crow's Nest Version 1.${data}`)
+        .setDescription(`**Name:** Bot in a Crow's Nest
         **Authors:** Zinnig#2769, Florian#8249
-        **Uptime:** ${client.uptime}`);
+        **Uptime:** ${utils.setupTimeDiff(client.uptime)===''? '<1min' : utils.setupTimeDiff(client.uptime)}`);
         message.channel.send(embed);
+        
+        
+       
     } 
 };

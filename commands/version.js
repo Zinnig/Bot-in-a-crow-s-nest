ef6779a029b7c77b86324e827d6aa3b3430238c4
Mahakadema <70533348+Mahakadema@users.noreply.github.com>
2021-06-14 17:38:49 +0200

    Zin's formatting annoys me

--- commands/version.js
+++ commands/version.js
@@ -11,13 +11,11 @@ module.exports = {
         const $ = cheerio.load(await fetch(`https://github.com/Zinnig/Bot-in-a-crow-s-nest`).then(r => r.text()));
         const data = $('body > div.application-main > div > main > div.container-xl > div > div > div.gutter-condensed > div.col-md-9 > div.Box > div.Box-header > div.js-details-container > div.flex-shrink-0 > ul > li > a > span > strong').text();
         const embed = new Discord.MessageEmbed()
-        .setTitle(`Bot in a Crow's Nest Version 1.${data}`)
+        .setTitle(`Bot in a Crow's Nest`)
         .setDescription(`**Name:** Bot in a Crow's Nest
         **Authors:** Zinnig#2769, Florian#8249
+	**Version:** 1.${data}
         **Uptime:** ${utils.setupTimeDiff(client.uptime)===''? '<1min' : utils.setupTimeDiff(client.uptime)}`);
         message.channel.send(embed);
-        
-        
-       
     } 
 };

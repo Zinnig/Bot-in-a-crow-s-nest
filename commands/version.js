b835853b3f27082805f8cc01ebcd97f19337999e
Mahakadema <70533348+Mahakadema@users.noreply.github.com>
2021-06-18 15:18:28 +0200

    fix aliases pt.15

--- commands/version.js
+++ commands/version.js
@@ -7,6 +7,7 @@ const fetch = require('node-fetch');
 module.exports = {
 	name: 'version',
 	description: "Gives general info about the bot",
+	aliases: ["ver", "debug"],
 	async execute(message, args, client) {
         const $ = cheerio.load(await fetch(`https://github.com/Zinnig/Bot-in-a-crow-s-nest`).then(r => r.text()));
         const data = $('body > div.application-main > div > main > div.container-xl > div > div > div.gutter-condensed > div.col-md-9 > div.Box > div.Box-header > div.js-details-container > div.flex-shrink-0 > ul > li > a > span > strong').text();

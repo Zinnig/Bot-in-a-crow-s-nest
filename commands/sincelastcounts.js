1619bfbf202f13347ddd3d3227576711ad40bac1
Mahakadema <70533348+Mahakadema@users.noreply.github.com>
2021-06-18 15:08:10 +0200

    fix aliases pt.10

--- commands/sincelastcounts.js
+++ commands/sincelastcounts.js
@@ -7,6 +7,7 @@ const Discord = require('discord.js');
 module.exports = {
 	name: 'sincelastcounts',
 	description: 'The amount of emeralds/gxp earned since the last Pillager Counts.',
+	aliases: [],
 	execute(message, args) {
     if(!message.member.roles.cache.some(role => role.name == "Guild Member") && !message.member.hasPermission("MANAGE_GUILD")) {message.channel.send(utils.errorResponse("notaguildmember")); return;}
         let outputStr = ""
